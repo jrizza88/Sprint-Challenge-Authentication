c3fcab0b97b09960a74cde5cfd5459bdaef25309
jrizza88 <jetorres88@gmail.com>
2019-04-05 23:52:33 -0400

    created app.js file and finished Registration component and set up jokes and login component

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {NavLink, Route} from 'react-router-dom';
 import './App.css';
+
+import Login from './Login/login';
+import Register from './Register/register';
+import Jokes from './Jokes/jokes';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+<h1 className="logo"> <NavLink to={"/"}>Jamar's dad joke app</NavLink></h1>
+      <header>
+        <nav>
+          <NavLink to={"/register"}>Register</NavLink>
+          &nbsp; | &nbsp;
+          <NavLink to={"/login"}>Login</NavLink>
+          &nbsp; | &nbsp;
+          <NavLink exact to={"/jokes"}>Dad Jokes</NavLink>
+          &nbsp; | &nbsp;
+          <button onClick={this.logout}>Logout</button>
+        </nav>
+      </header>
+      <main>
+        <Route path="/register" component={Register}/>
+        <Route path="/login" component={Login} />
+        <Route path="/jokes" component={Jokes} />
+      </main>
+      </>
     );
+  }
+
+  logout = () => {
+    localStorage.removeItem("jwt");
+    this.props.history.push('/login')
   }
 }
 
---
+++ client/src/Jokes/jokes.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import axios from 'axios';
+
+
+
+class Jokes extends React.Component {
+    state = {
+        jokes: [],
+    };
+
+    render() {
+        if (!this.state.jokes) {
+            return <div>Only registered users can view!</div>
+        }
+        return (
+            <>
+                <h2>Jokes</h2>
+                    <ul>{this.state.jokes.map( j => (
+                        <li key={j.id}>{j.joke}</li>
+                    ))}
+                </ul>
+            </>
+        )
+    }
+
+    
+
+   componentDidMount(){
+      
+      
+        const headers = {Authorization: localStorage.getItem('jwt') };
+        const endpoint = 'http://localhost:3300/api/jokes';
+
+        axios.get(endpoint, headers)
+        .then (res => {
+            this.setState({ jokes: res.data})
+        }).catch(e => {
+            console.error(e);
+        })
+    };
+}
+
+export default Jokes;
